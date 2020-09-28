cf1567674de6918dbb5624f8381b18f5d411dd60
wangsifeng <wangsifeng@leshu.com>
2020-09-28 16:03:51 +0800

    Initial commit

---
+++ app/src/main/java/com/leshu/msa_mdid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.leshu.msa_mdid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
